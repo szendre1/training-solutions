e11070ddeae84c3ed5506bbc5093c43a50c63e10
szendre1 <endre.szarka@gmail.com>
2020-10-31 23:17:56 +0100

    Store

--- src/main/java/classsctructureconstructors/Store.java
+++ src/main/java/classsctructureconstructors/Store.java
@@ -6,7 +6,7 @@ public class Store {
 
     public Store(String product) {
         this.product = product;
-        this.stock = 0;
+        this.stock = 0; //
     }
 
     public void store(int stock){
