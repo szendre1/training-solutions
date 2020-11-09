6fee7d2bd13f88f7f58d440784e13fa689d7bf2a
szendre1 <endre.szarka@gmail.com>
2020-11-09 10:11:43 +0100

    projectmunka, idáig jutottam eddig

--- src/main/java/week02/Controller.java
+++ src/main/java/week02/Controller.java
@@ -1,9 +1,34 @@
 package week02;
 
+import java.util.Scanner;
+
 public class Controller {
     public static void main(String[] args) {
         Office office = new Office();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Irodaház nyilvántartó program");
+        System.out.print("Hány darab tárgyalót szeretnél rögzíteni? ");
+        int targyaloDb = scanner.nextInt();
+        scanner.nextLine();
 
+        String tName;
+        int tLength;
+        int tWidth;
+        for (int i=1;i<=targyaloDb;i++){
+            MeetingRoom meetingRoomNew = new MeetingRoom("Elso",2,3);
+            System.out.print("Tárgyaló neve: ");
+            meetingRoomNew.setName(scanner.nextLine());
+            System.out.print("Tárgyaló hossza: ");
+            meetingRoomNew.setLength(scanner.nextInt());
+            scanner.nextLine();
+            System.out.print("Tárgyaló szélessége: ");
+            meetingRoomNew.setWidth(scanner.nextInt());
+            scanner.nextLine();
+            System.out.println();
+            office.addMeetingRoom(meetingRoomNew);
+        }
+
+        /*
         MeetingRoom meetingRoom1 = new MeetingRoom("Elso",2,3);
         MeetingRoom meetingRoom2 = new MeetingRoom("Masodik",4,5);
         MeetingRoom meetingRoom3 = new MeetingRoom("Harmadik",6,7);
@@ -13,6 +38,10 @@ public class Controller {
         office.addMeetingRoom(meetingRoom2);
         office.addMeetingRoom(meetingRoom3);
         office.addMeetingRoom(meetingRoom4);
+
+
+
+         */
 
         office.printNames();
         System.out.println();
