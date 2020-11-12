00cfe3ab4f0230dfd83b447f504a5b175dda785e
szendre1 <endre.szarka@gmail.com>
2020-11-12 23:35:36 +0100

    Greerings

--- src/main/java/controlselection/greetings/Greetings.java
+++ src/main/java/controlselection/greetings/Greetings.java
@@ -20,18 +20,4 @@ public class Greetings {
         return hello;
     }
 
-/*
-    assertEquals("jó éjt", greetings.greet(4, 30));
-    assertEquals("jó éjt", greetings.greet(5, 0));
-    assertEquals("jó reggelt", greetings.greet(5, 1));
-    assertEquals("jó reggelt", greetings.greet(9, 0));
-    assertEquals("jó napot", greetings.greet(9, 1));
-    assertEquals("jó napot", greetings.greet(18, 30));
-    assertEquals("jó estét", greetings.greet(18, 31));
-    assertEquals("jó estét", greetings.greet(20, 0));
-    assertEquals("jó éjt", greetings.greet(20, 1));
-
-
- */
-
 }
