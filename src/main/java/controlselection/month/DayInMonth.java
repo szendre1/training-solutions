291a3c72f77f19d2f8112c13e3310b4da7c72803
szendre1 <endre.szarka@gmail.com>
2020-11-14 20:40:44 +0100

    2020.11.04

--- src/main/java/controlselection/month/DayInMonth.java
+++ src/main/java/controlselection/month/DayInMonth.java
@@ -2,6 +2,26 @@ package controlselection.month;
 
 public class DayInMonth {
     public int numberOfDays(int year, String month) {
+        int x;
+        switch (month.toLowerCase()) {
+            case "január", "március", "május",
+                    "július", "augusztus", "október", "december" -> x=31;
+            case "április", "június", "szeptember", "november" -> x=30;
+            case "február" -> {
+                if ((year % 4) == 0) {
+                    if (year % 100 == 0) {
+                        if (year % 400 == 0) {
+                            x=29;
+                        } else x=28;
+                    } else x=29;
+                } else x=28;
+            }
+            default -> { throw new IllegalArgumentException("Invalid month: " + month); }
+        }  // switch
+
+
+
+        /*
         switch (month.toLowerCase()) {
             case "január":
             case "március":
@@ -30,6 +50,7 @@ public class DayInMonth {
             default:
                 throw new IllegalArgumentException("Invalid month: " + month);
         }  // switch
-
+         */
+    return x;
     }
 }
