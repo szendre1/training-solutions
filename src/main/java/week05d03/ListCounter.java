83176b0d55442db0097966e9e3f24368f93be9a5
szendre1 <endre.szarka@gmail.com>
2020-11-25 13:05:53 +0100

    ListCounter

---
+++ src/main/java/week05d03/ListCounter.java
@@ -0,0 +1,29 @@
+package week05d03;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListCounter {
+
+    public int Counter(List<String> myList){
+        int talalat = 0;
+        for (String x:myList) {
+            if (x.toUpperCase().charAt(0)=='A'){
+                talalat++;
+            }
+        }
+        return talalat;
+    }
+
+    public static void main(String[] args) {
+        ListCounter listCounter = new ListCounter();
+        List<String> myList = new ArrayList<>();
+        myList.add("Az első");
+        myList.add("a második");
+        myList.add("harmadik");
+        System.out.println(""+listCounter.Counter(myList));
+
+    }
+
+}
