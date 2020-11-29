7768c4b32c3240eeb9dcb4f01ba6de07784e0b67
szendre1 <endre.szarka@gmail.com>
2020-11-29 18:34:22 +0100

    PrintFormat

--- src/main/java/formatlocaleprintf/PrintFormat.java
+++ src/main/java/formatlocaleprintf/PrintFormat.java
@@ -18,22 +18,22 @@ public class PrintFormat {
 
     }
 
-        public String printFormattedText(Double number){
-            return String.format("Total is: %,.2f Ft",number);
-        }
+    public String printFormattedText(Double number){
+        return String.format("Total is: %,.2f Ft",number);
+    }
 
-        public String printFormattedText(int count, String fruit){
-            return String.format("We counted %d %s in the basket",count,fruit);
-        }
+    public String printFormattedText(int count, String fruit){
+        return String.format("We counted %d %s in the basket",count,fruit);
+    }
 
-        public String printFormattedText(int number){
-            return String.format("Right edge of numbers set at 4 spaces from text:%4d",number,number);
-        }
+    public String printFormattedText(int number){
+        return String.format("Right edge of numbers set at 4 spaces from text:%4d",number,number);
+    }
 
 
-        public String printFormattedText(Integer i, Integer j, Integer k){
-            return String.format("Multiple objects containing text:%d\t%d\t%d",i,j,k);
-        }
+    public String printFormattedText(Integer i, Integer j, Integer k){
+        return String.format("Multiple objects containing text:%d\t%d\t%d",i,j,k);
+    }
 
 
 }
