643ae85616c0ceeb07f51a2720b459b584937616
szendre1 <endre.szarka@gmail.com>
2020-11-30 13:15:39 +0100

    ListSelector

---
+++ src/main/java/week06d01/ListSelector.java
@@ -0,0 +1,44 @@
+package week06d01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListSelector {
+
+
+    public String ListSelector(List<String> lista) {
+        if (lista == null) {
+            throw new IllegalArgumentException("Nem adtál meg listát!!!");
+        }
+        if (lista.size() == 0) {
+            return "";
+        }
+        StringBuilder s = new StringBuilder("");
+        s.append("[");
+        for (int x = 0; lista.size() > x; x++) {
+            if (x % 2 == 0) {
+                s.append(lista.get((int) x));
+            }
+        }
+        s.append("] ");
+
+
+        s.append("[");
+        for (int x = 0; lista.size() > x; x++) {
+            if (x % 2 == 1) {
+                s.append(lista.get((int) x) + ",");
+            }
+        }
+        StringBuilder s1 = new StringBuilder();
+        s1.append(s.substring(0, s.length() - 1));
+        s1.append("]");
+        return (s1.toString());
+    }
+
+
+    public static void main(String[] args) {
+        ListSelector ls = new ListSelector();
+        String stringToList = ls.ListSelector(Arrays.asList("Egy", "Kettő", "Három", "Négy", "Öt", "Hat", "Hét"));
+        System.out.println(stringToList);
+    }
+}
