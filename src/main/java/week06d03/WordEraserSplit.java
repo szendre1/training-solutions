68a846b36e5bc0d3de5f48f5a335e420f80b08bd
szendre1 <endre.szarka@gmail.com>
2020-12-02 16:10:27 +0100

    WordEraser

--- src/main/java/week06d03/WordEraserSplit.java
+++ src/main/java/week06d03/WordEraserSplit.java
@@ -6,14 +6,15 @@ import java.util.List;
 
 public class WordEraserSplit {
 
+    private boolean isEmpty(String word){
+        return word == null || word.trim().equals("");
+    }
+
     public String eraserWord(String words, String word) {
         if (words == null) {
             throw new IllegalArgumentException("Hibás az első kifejezés!");
         }
-        if (word == null) {
-            return words;
-        }
-        if (word.trim().equals("")) {
+        if (isEmpty(word)) {
             return words;
         }
         List<String> wordList = new ArrayList<>(Arrays.asList(words.split(" ")));
