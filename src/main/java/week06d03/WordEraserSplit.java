0687327b6d84fadc96054679fd18df214f5c326b
szendre1 <endre.szarka@gmail.com>
2020-12-02 15:21:42 +0100

    WordEraser

---
+++ src/main/java/week06d03/WordEraserSplit.java
@@ -0,0 +1,28 @@
+package week06d03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WordEraserSplit {
+
+    public String eraseWord(String words, String word) {
+        if (words == null) {
+            throw new IllegalArgumentException("Hibás az első kifejezés!");
+        }
+        if (word == null) {
+            return words;
+        }
+        if (word.trim().equals("")) {
+            return words;
+        }
+        List<String> wordList = new ArrayList<>(Arrays.asList(words.split(" ")));
+        String newWords = "";
+        for (String x : wordList) {
+            if (!x.equals(word.trim())) {
+                newWords = newWords + x + " ";
+            }
+        }
+        return newWords.trim();
+    }
+}
