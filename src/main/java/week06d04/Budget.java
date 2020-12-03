b08148b6d947a88335ba5206891df04de8d85651
szendre1 <endre.szarka@gmail.com>
2020-12-03 13:22:15 +0100

    Budget

--- src/main/java/week06d04/Budget.java
+++ src/main/java/week06d04/Budget.java
@@ -11,6 +11,9 @@ public class Budget {
     }
 
     public List<Item> getItemsByMonth(int month){
+        if (month < 1 || month > 12){
+            throw new IllegalArgumentException("Month need to be between 1 and 12");
+        }
         List<Item> monthList = new ArrayList();
         for (Item x:mylist){
             if (x.getMonth()==month){
