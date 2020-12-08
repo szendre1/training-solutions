448ee2bcbaf6d10bc5127b5063984306fd86e8af
szendre1 <endre.szarka@gmail.com>
2020-12-08 13:23:57 +0100

    DigitSum

--- src/main/java/week07d02/DigitSum.java
+++ src/main/java/week07d02/DigitSum.java
@@ -2,12 +2,13 @@ package week07d02;
 
 public class DigitSum {
 
-    public static int sumOfDigits(int... x){
-        int sum = 0;
-        for (int y:x){
-            sum=sum+y;
+    public static int sumOfDigits(int x) {
+        int collector = 0;
+        String x1 = Integer.toString(x);
+        for (int i = 0; i < x1.length();i++){
+            collector = collector + Integer.valueOf(x1.substring(i, i + 1));
         }
-        return sum;
+        return collector;
     }
 
 }
