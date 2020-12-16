1927ec2147627c30fc455c4b462903876ee52ac3
szendre1 <endre.szarka@gmail.com>
2020-12-16 13:16:23 +0100

    String union

--- src/main/java/week08d03/StringLists.java
+++ src/main/java/week08d03/StringLists.java
@@ -7,7 +7,7 @@ public class StringLists {
     public List<String> stringListsUnion(List<String> first, List<String> second) {
         List<String> newList = new ArrayList<>(first);
         newList.addAll(second);
-        return new ArrayList<>(new HashSet<>());
+        return new ArrayList<>(new HashSet<>(newList));
     }
 
 }
