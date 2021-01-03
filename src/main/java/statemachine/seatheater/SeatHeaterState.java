5143f69d9fa54036842bb178971a03277774a3ca
szendre1 <endre.szarka@gmail.com>
2021-01-03 22:42:58 +0100

    enumabstract

---
+++ src/main/java/statemachine/seatheater/SeatHeaterState.java
@@ -0,0 +1,30 @@
+package statemachine.seatheater;
+
+public enum SeatHeaterState {
+    ONE{
+        public SeatHeaterState next(){
+            return SeatHeaterState.OFF;
+        }
+    },
+    TWO{
+        public SeatHeaterState next(){
+            return SeatHeaterState.ONE;
+        }
+
+    }
+    ,
+    THREE{
+        public SeatHeaterState next(){
+            return SeatHeaterState.TWO;
+        }
+
+    },
+    OFF{
+        public SeatHeaterState next(){
+            return SeatHeaterState.THREE;
+        }
+
+    };
+
+    abstract SeatHeaterState next();
+}
