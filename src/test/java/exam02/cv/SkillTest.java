aad669a312a0c87c11595d3e28fe56562e8a6bee
szendre1 <endre.szarka@gmail.com>
2021-01-06 18:14:38 +0100

    exam02

---
+++ src/test/java/exam02/cv/SkillTest.java
@@ -0,0 +1,14 @@
+package exam02.cv;
+
+public class SkillTest {
+/*
+    @Test
+    public void testCreate() {
+        Skill skill = new Skill("programming", 3);
+        assertEquals("programming", skill.getName());
+        assertEquals(3, skill.getLevel());
+    }
+
+
+ */
+}
