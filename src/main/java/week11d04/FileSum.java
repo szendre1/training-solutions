e7297db336850ccb6bd0c319bff0e4cad0665355
szendre1 <endre.szarka@gmail.com>
2021-01-14 14:23:24 +0100

    FileSum

--- src/main/java/week11d04/FileSum.java
+++ src/main/java/week11d04/FileSum.java
@@ -10,25 +10,24 @@ public class FileSum {
         int readNumber=0;
         int sumNumber=0;
 
+
         String fileName;
         for (int actualFile = 0; actualFile < 100; actualFile++) {
-            fileName = "number";
-            if (actualFile < 10) {
-                fileName += "0";
-            }
+        //    fileName = "number";
+        //    if (actualFile < 10) {
+        //        fileName += "0";
+        //    }
 
-            fileName += String.valueOf(actualFile) + ".txt";
+            fileName = String.format("number%02d.txt",actualFile);
             Path file = Path.of(fileName);
-            System.out.println(file);
             try {
                 if (Files.isRegularFile(file)) {
-                    readNumber = Integer.parseInt(Files.readString(file));
+                    sumNumber += Integer.parseInt(Files.readString(file));
+                    //System.out.println(readNumber);
                 }
             } catch (IOException ioe) {
                 throw new IllegalStateException("Can not read file", ioe);
             }
-
-        sumNumber += readNumber;
         }
 
     return sumNumber;
