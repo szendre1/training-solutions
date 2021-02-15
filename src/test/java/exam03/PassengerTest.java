8344a22de771319d5ea6e32da9f70b4d82b6a27e
szendre1 <endre.szarka@gmail.com>
2021-02-15 18:34:23 +0100

    Boat

--- src/test/java/exam03/PassengerTest.java
+++ src/test/java/exam03/PassengerTest.java
@@ -1,15 +1,16 @@
 package exam03;
 
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PassengerTest {
 
     @Test
     void create() {
         Passenger passenger = new Passenger("John Doe", CruiseClass.LUXURY);
-        Assertions.assertEquals("John Doe", passenger.getName());
-        Assertions.assertEquals(CruiseClass.LUXURY, passenger.getCruiseClass());
+        assertEquals("John Doe", passenger.getName());
+        assertEquals(CruiseClass.LUXURY, passenger.getCruiseClass());
     }
 }
